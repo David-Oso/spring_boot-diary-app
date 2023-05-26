35e0593b07b8b6ed03c499e1a950046a87b024cc
David <osodavid001@gmail.com>
2023-05-26 06:27:48 +0100

    entry commit

--- src/main/java/com/example/SpringBootDiaryApp/services/diaryService/DiaryServiceImpl.java
+++ src/main/java/com/example/SpringBootDiaryApp/services/diaryService/DiaryServiceImpl.java
@@ -1,4 +1,16 @@
 package com.example.SpringBootDiaryApp.services.diaryService;
 
-public class DiaryServiceImpl {
+import com.example.SpringBootDiaryApp.data.model.Diary;
+import com.example.SpringBootDiaryApp.data.repositories.DiaryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DiaryServiceImpl implements DiaryService{
+    private final DiaryRepository diaryRepository;
+    @Override
+    public Diary saveDiary(Diary diary) {
+        return diaryRepository.save(diary);
+    }
 }
